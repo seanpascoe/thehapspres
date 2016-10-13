09c879f60baecb09af8c406c833a19a73381c693
Taylor Wu <Taylorwu21@gmail.com>
2016-10-12 19:19:38 -0600

    added images to our tech sack

--- presentation/index.js
+++ presentation/index.js
@@ -42,21 +42,27 @@ const images = {
   road: require("../assets/road.jpg"),
   roadblock: require("../assets/roadblock.jpg"),
   concert: require("../assets/concert.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png"),
-  iphone: require("../assets/iphone.png"),
   addEvent: require("../assets/add_event.gif"),
   detailview: require("../assets/detailview.gif"),
   filter: require("../assets/filter.gif"),
   listview: require("../assets/listview.gif"),
-  mapview: require("../assets/mapview.gif")
+  mapview: require("../assets/mapview.gif"),
+  github: require("../assets/github.png"),
+  googlemap: require("../assets/googlemap.png"),
+  heroku: require("../assets/heroku.png"),
+  js: require("../assets/js.png"),
+  materialize: require("../assets/materialize.png"),
+  momentjs: require("../assets/momentjs.png"),
+  mongodb: require("../assets/mongodb.png"),
+  nodejs: require("../assets/nodejs.png"),
+  react: require("../assets/react.png"),
+  redux: require("../assets/redux.png")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: '#40c4ff',
+  primary: "#40c4ff",
   // primary: "#3498DB",
   secondary: "#2C3E50"
 });
@@ -66,7 +72,7 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="secondary" style={{paddingTop: '0'}}>
+          <Slide transition={["zoom"]} bgColor="secondary" style={{paddingTop: "0"}}>
             <Image src={images.thehaps} height="293px"/>
             <Heading size={1} fit textColor="primary">
               Local events in your hand!
@@ -94,15 +100,14 @@ export default class Presentation extends React.Component {
               The Solution:
             </Heading>
             <BlockQuote>
-              <Quote>A central, web-based application with multiple views and a clean, snappy UI.</Quote>
-              <Cite>God</Cite>
+              <Quote>The haps is a central, web-based application with multiple views and a clean, snappy UI.</Quote>
             </BlockQuote>
           </Slide>
           <Slide transition={["slide"]} bgColor="white">
             <Layout>
               <Heading textColor="secondary" textFont="primary" textAlign="left" >
                 FEATURES:
-                <Heading size={2} textColor="primary" textFont="primary" align='flex-start flex-start' textAlign="left">
+                <Heading size={2} textColor="primary" textFont="primary" align="flex-start flex-start" textAlign="left">
                   Map View
                   <List textColor="secondary" style={{lineHeight: '1.5', marginTop: '30px', marginRight: "20px", listStylePosition: "outside", paddingLeft: "40px"}}>
                     <Appear><ListItem>Google Maps JS API</ListItem></Appear>
@@ -122,7 +127,7 @@ export default class Presentation extends React.Component {
                   List View
                   <List textColor="primary" style={{lineHeight: '1.5', marginTop: '30px', marginRight: "20px", listStylePosition: "outside", paddingLeft: "40px"}}>
                     <Appear><ListItem>Instant Toggle List/Map</ListItem></Appear>
-                    <Appear><ListItem>Sorted by Time Value</ListItem></Appear>
+                    <Appear><ListItem>Sorted by Time</ListItem></Appear>
                     <Appear><ListItem>3 Spots to Access Filter</ListItem></Appear>
                   </List>
                 </Heading>
@@ -153,7 +158,7 @@ export default class Presentation extends React.Component {
                 <Heading size={2} textColor="secondary" textFont="primary" align='flex-start flex-start' textAlign="left">
                   Filter
                   <List textColor="primary" style={{lineHeight: '1.5', marginTop: '30px', marginRight: "20px", listStylePosition: "outside", paddingLeft: "40px"}}>
-                    <Appear><ListItem>Filter by date range - datepicker</ListItem></Appear>
+                    <Appear><ListItem>Filter by date</ListItem></Appear>
                     <Appear><ListItem>Filter by category</ListItem></Appear>
                   </List>
                 </Heading>
@@ -182,8 +187,8 @@ export default class Presentation extends React.Component {
             </Heading>
             <List textColor="white">
               <Appear><ListItem>Lame google maps api packages. Un-stateful!</ListItem></Appear>
-              <Appear><ListItem>Avoiding map reloads.</ListItem></Appear>
-              <Appear><ListItem>Markers at same location, clustering.</ListItem></Appear>
+              <Appear><ListItem>Avoiding map reloads</ListItem></Appear>
+              <Appear><ListItem>Markers at same location, clustering</ListItem></Appear>
             </List>
           </Slide>
           <Slide transition={["slide"]} bgImage={images.road} bgDarken={0.75}>
@@ -197,10 +202,20 @@ export default class Presentation extends React.Component {
               <Appear><ListItem>Upvoting, Tags, & Comments</ListItem></Appear>
             </List>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
+          <Slide transition={["slide"]} bgColor="secondary" style={{paddingLeft: "15px !important", paddingRight: "15px !important"}}>
+            <Image src={images.js} height="170px" style={{margin: "10px"}} />
+            <Image src={images.github} height="170px" style={{margin: "10px"}} />
+            <Image src={images.googlemap} height="170px" style={{margin: "10px"}} />
+            <Image src={images.heroku} height="170px" style={{margin: "10px"}} />
+            <Image src={images.materialize} height="170px" style={{margin: "10px"}} />
+            <Heading size={2} caps fit textColor="primary" textFont="primary" style={{padding: "10px"}}>
               Technology Stack
             </Heading>
+            <Image src={images.momentjs} height="170px" style={{margin: "10px"}}/>
+            <Image src={images.mongodb} height="170px" style={{margin: "10px"}}/>
+            <Image src={images.nodejs} height="170px" style={{margin: "10px"}}/>
+            <Image src={images.react} height="170px" style={{margin: "10px"}}/>
+            <Image src={images.redux} height="170px" style={{margin: "10px"}}/>
           </Slide>
           <Slide transition={["zoom"]} bgColor="secondary">
             <Image src={images.thehaps} height="293px"/>
@@ -208,96 +223,6 @@ export default class Presentation extends React.Component {
               http://thehapsmap.com
             </Heading>
           </Slide>
-          {/* <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.concert} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
-          </Slide> */}
         </Deck>
       </Spectacle>
     );
